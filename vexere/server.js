efb096e29b91a1b53305bb1a320e590236554193
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-05-14 15:18:20 +0700

    init Ve Xe Re project

---
+++ vexere/server.js
@@ -0,0 +1,24 @@
+// import library
+const express = require('express');
+const path = require('path');
+const { sequelize } = require('./models');
+
+// init application with express
+const app = express()
+
+// convert input data to type JSON
+app.use(express.json())
+
+// setup path static file for server to save file and image
+const publicDirPath = path.join(__dirname, './public') // return url to public file
+app.use(express.static(publicDirPath))
+
+app.listen(3000, async () => {
+  console.log('App listening on http://localhost:3000')
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})
